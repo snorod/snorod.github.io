75c461827f964c15e1de2758ccb6ba3c0f1cd3f3
Stephen Norod <stephennorod@yahoo.com>
2018-01-30 02:41:06 -0500

    help

---
+++ js/resources.js
@@ -0,0 +1,162 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "area01_bkg0",
+        "type": "image",
+        "src": "data/img/area01_bkg0.png"
+    },
+    {
+        "name": "area01_bkg1",
+        "type": "image",
+        "src": "data/img/area01_bkg1.png"
+    },
+    {
+        "name": "background_original",
+        "type": "image",
+        "src": "data/img/background_original.png"
+    },
+    {
+        "name": "background",
+        "type": "image",
+        "src": "data/img/background.png"
+    },
+    {
+        "name": "clouds",
+        "type": "image",
+        "src": "data/img/clouds.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "city_bg",
+        "type": "image",
+        "src": "data/img/map/city_tileset/city_bg.png"
+    },
+    {
+        "name": "city",
+        "type": "image",
+        "src": "data/img/map/city_tileset/city.png"
+    },
+    {
+        "name": "abbott",
+        "type": "image",
+        "src": "data/img/sprite/abbott.png"
+    },
+    {
+        "name": "batman_test",
+        "type": "image",
+        "src": "data/img/sprite/batman_test.png"
+    },
+    {
+        "name": "flash_run_right",
+        "type": "image",
+        "src": "data/img/sprite/flash_run_right.png"
+    },
+    {
+        "name": "flash",
+        "type": "image",
+        "src": "data/img/sprite/flash.png"
+    },
+    {
+        "name": "gripe_run_right - Copy",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right - Copy.png"
+    },
+    {
+        "name": "gripe_run_right_bat",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right_bat.png"
+    },
+    {
+        "name": "gripe_run_right_batOrig",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right_batOrig.png"
+    },
+    {
+        "name": "gripe_run_right_L",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right_L.png"
+    },
+    {
+        "name": "gripe_run_right_trans",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right_trans.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "tech_tower",
+        "type": "image",
+        "src": "data/img/sprite/tech_tower.png"
+    },
+    {
+        "name": "wheelie_right",
+        "type": "image",
+        "src": "data/img/sprite/wheelie_right.png"
+    },
+    {
+        "name": "tech_tower_orig",
+        "type": "image",
+        "src": "data/img/tech_tower_orig.png"
+    },
+    {
+        "name": "tech_tower",
+        "type": "image",
+        "src": "data/img/tech_tower.png"
+    },
+    {
+        "name": "tileset",
+        "type": "image",
+        "src": "data/img/tileset.png"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.tmx"
+    },
+    {
+        "name": "area02",
+        "type": "tmx",
+        "src": "data/map/area02.tmx"
+    },
+    {
+        "name": "area03",
+        "type": "tmx",
+        "src": "data/map/area03.tmx"
+    }
+];
