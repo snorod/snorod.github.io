8a8236c0592916eeaac9cc6d68bfa2c6d5254ab5
Stephen Norod <stephennorod@yahoo.com>
2018-01-30 02:12:09 -0500

    melonJS

---
+++ js/game.js
@@ -0,0 +1,54 @@
+
+/* Game namespace */
+var game = {
+
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
+
+
+    // Run on page load.
+    "onload" : function () {
+        // Initialize the video.
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+
+        // add "#debug" to the URL to enable the debug Panel
+        if (me.game.HASH.debug === true) {
+          window.onReady(function () {
+            me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+          });
+        }
+
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+
+        // set and load all resources.
+        // (this will also automatically switch to the loading screen)
+        me.loader.preload(game.resources, this.loaded.bind(this));
+
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+
+        // add our player entity in the entity pool
+        me.pool.register("mainPlayer", game.PlayerEntity);
+
+        // enable the keyboard
+          me.input.bindKey(me.input.KEY.LEFT,  "left");
+          me.input.bindKey(me.input.KEY.RIGHT, "right");
+          me.input.bindKey(me.input.KEY.SPACE,     "jump", true);
+
+        // Start the game.
+        me.state.change(me.state.PLAY);
+    }
+};
