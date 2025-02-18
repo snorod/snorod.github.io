889bb7a22f4912ba915add5eb0983a78c54503b8
Stephen Norod <snorod@stephens-air.myfiosgateway.com>
2025-02-17 19:49:19 -0500

    Buildings

---
+++ data/map/city_bg_original.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="city_bg_original" tilewidth="20" tileheight="20" tilecount="72" columns="8">
+ <image source="../img/map/city_tileset/city_bg_original.png" width="160" height="180"/>
+</tileset>
